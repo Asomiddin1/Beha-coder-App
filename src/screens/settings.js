474dba83d2dd01114af2b021d9cb8c664139435d
Asomiddin <asomiddin320@gmail.com>
2023-11-05 14:59:04 -0800

    helo problem run gradlew

--- src/screens/settings.js
+++ src/screens/settings.js
@@ -1,14 +1,38 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useCallback } from 'react'
+import { View, Text, TouchableOpacity, Button , Linking } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import Intro from '../components/intro';
 
 const Settings = () => {
     const navigation = useNavigation()
+    const  url = 'https://t.me/asomiddin_004'
+
+    
     return (
         <View className='flex-1 bg-slate-900  justify-center items-center'>
-             <Intro />
+           <View>
+               <View>
+                   {/* Profle image */}
+               </View>
+               <View>
+   
+       
+
+                    <View className='flex-row gap-2 mb-4  items-center'>
+                        <Ionicons name='cog-sharp' color={'white'} size={40} />
+                        <Text className='text-white  text-[27px] '>Settings</Text>
+                    </View>
+                    <TouchableOpacity className='flex-row gap-2 mb-4 tems-center'>
+                        <Ionicons name='help-buoy-sharp' color={'white'} size={40} />
+                        <Text onPress={()=> Linking.openURL('https://t.me/asomiddin_004')} className='text-white  text-[27px] '>Help</Text>
+                    </TouchableOpacity>
+
+                    <View className='flex-row gap-2 tems-center'>
+                        <Ionicons name='log-out-sharp' color={'white'} size={40} />
+                        <Text className='text-white ml-4  text-[27px] '>Loguot</Text>
+                    </View>
+               </View>
+           </View>
               
              <View className='absolute  mx-[5%] mb-[8%] flex-row justify-between items-center px-4 rounded-[20px] bottom-0 w-[90%] h-[10vh] bg-slate-800'>
           <TouchableOpacity className='flex-col justify-center items-center' onPress={()=> navigation.navigate('Home')}>
